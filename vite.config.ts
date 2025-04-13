f0aeebd43591b91f74b27fed921d6f4ecc1526c2
CUPID <archith837@gmail.com>
2025-04-13 16:13:07 +0530

    fix bugus

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -12,24 +10,9 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     emptyOutDir: true,
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'ui-vendor': ['@radix-ui/react-dialog', '@radix-ui/react-toast', '@radix-ui/react-tooltip']
-        },
-      },
-    },
   },
   server: {
     port: 3000,
-    strictPort: true
-  },
-  preview: {
-    port: 3000,
-    strictPort: true
   }
 });
